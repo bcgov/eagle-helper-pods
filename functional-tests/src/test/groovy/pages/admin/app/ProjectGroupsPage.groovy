9fb84ad8aea81ce285a5935bb9963348b4020ca5
Morgan Thompson <morgan.thompson@gov.bc.ca>
2019-09-11 17:03:25 -0700

    EE-241: add inital implementation of admin and public pages

---
+++ functional-tests/src/test/groovy/pages/admin/app/ProjectGroupsPage.groovy
@@ -0,0 +1,47 @@
+package Pages.Admin
+
+import Admin.modules.WorkingGroupTableRows
+import geb.waiting.WaitTimeoutException
+
+class ProjectGroupssPage extends BaseAppPage {
+  static at = {}
+  static content = {
+    selectAllButton { $('#button-sa') }
+    addButton { $('#button-a') }
+    editButton { $('#button-e') }
+    deleteButton { $('#button-d') }
+    exportButton { $('#button-ex') }
+    copyEmailsButton { $('#button-ce') }
+    workingGroupList {
+        $('table tr').tail().moduleList(WorkingGroupTableRows) // tailing to skip header row 
+    }
+  }
+  
+  void clickWorkingGroup() {
+    workingGroupList[0].clickCell()
+  }
+
+  void selectAll() {
+    selectAllButton.click()
+  }
+
+  void addGroup() {
+    addButton.click()
+  }
+
+  void editGroup() {
+    editButton.click()
+  }
+
+  void deleteGroup() {
+    deleteButton.click()
+  }
+
+  void exportGroup() {
+    exportButton.click()
+  }
+
+  void copyEmails() {
+    copyEmailsButton.click()
+  }
+}
---
+++ functional-tests/src/test/groovy/pages/admin/app/ProjectListPage.groovy
@@ -0,0 +1,37 @@
+package Pages.Admin
+
+import Admin.modules.TableRows
+import geb.waiting.WaitTimeoutException
+
+class ProjectListPage extends BaseAppPage {
+  static at ={ }
+  static url = '/projects'
+  static content = {
+    // todo get a count of projects?
+    projectList {
+        $('table tr').tail().moduleList(TableRows) // tailing to skip header row 
+    }
+  }
+ 
+
+  Boolean verifyTitle() {
+    pageTitle.text() == expectedTitle
+  }
+
+  void clickSearchButton() {
+    closeButton.click()
+  }
+
+  void clickAdvancedSearchButton() {
+    advancedSearchButton.click()
+  }
+
+  void setSearchTerms(String searchTerms) {
+    searchField.value(searchTerms)
+  }
+
+// todo verify what we click is the same each time
+  void clickProjectLink() {
+      projectList[0].clickCell()
+  }
+}
